aa72f814032674cfd60526ce1eac5a7b73613f5d
rjgc2008 <969888063@qq.com>
2022-03-24 20:48:53 +0800

    updates\

---
+++ src/chapterone/PracticeOne.java
@@ -0,0 +1,11 @@
+package chapterone;
+
+public class PracticeOne {
+    public void oneOneOne(){
+        System.out.println("( 0 + 15) / 2 = " + ((0 + 15) / 2) );
+        System.out.println((0 + 15) / 2);
+    }
+    public static  void main(String[] args){
+        new PracticeOne().oneOneOne();
+    }
+}
