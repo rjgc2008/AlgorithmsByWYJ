77184ae777f9e3f86b95dfce22c7730afb4f3622
rjgc2008 <969888063@qq.com>
2022-03-25 10:35:18 +0800

    update

---
+++ src/chapterone/ChapterOne.java
@@ -0,0 +1,24 @@
+package chapterone;
+
+public class ChapterOne {
+    /**
+     * 欧几里得算法，求最大公约数
+     * gcd(greatest common divisor)
+     * @param p
+     * @param q
+     * @return
+     */
+    public static int gcd(int p, int q){
+        if( q == 0)   return p;
+        return gcd(q, p % q);
+    }
+
+    public void oneOneOne(){
+        System.out.println("( 0 + 15) / 2 = " + ((0 + 15) / 2) );
+        System.out.println((0 + 15) / 2);
+    }
+
+    public static  void main(String[] args){
+        new ChapterOne().oneOneOne();
+    }
+}
